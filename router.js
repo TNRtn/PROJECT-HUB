2d2b3f8103464cb9f7fe3373b9de4d8b44c51cac
TNRtn <137277969+TNRtn@users.noreply.github.com>
2024-07-21 16:45:40 +0530

    Update router.js

--- router.js
+++ router.js
@@ -9,4 +9,6 @@ router.get('/ufetch',middleware,uapi.uall);
 router.get('/myprofile',middleware,uapi.me);
 router.post('/addreview',middleware,review.addreview)
 router.get('/myreview',middleware,review.myreview)
+router.get('/allreviews',middleware,review.allreviews)
+router.post('/addskills',middleware,uapi.addskills)
 module.exports=router;
